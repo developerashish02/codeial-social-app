3b27745193f5e13561c247a31a3220c561d4b7b7
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-16 21:03:40 +0530

    set up view

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const port = 9001;
 const app = express();
 
-// use express router 
-app.use('/', require('./routes'))
+// use express router
+app.use("/", require("./routes"));
+
+// view engine
+app.set("view engine", "ejs");
 
 app.listen(port, (err) => {
 	if (err) {
