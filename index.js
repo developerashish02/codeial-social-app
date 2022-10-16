d89c4a0dfcea100792e53dcd0f9502e082e7c491
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-16 20:43:09 +0530

    Add routes and controllers

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const port = 9001;
 const app = express();
 
+// use express router 
+app.use('/', require('./routes'))
+
 app.listen(port, (err) => {
 	if (err) {
 		console.log(`error while setting up server on port ${port}`);
