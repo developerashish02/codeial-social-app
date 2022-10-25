acfabcab256a63a368408ee9f2eb2b37be3426de
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-25 17:30:00 +0530

    populated user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,32 @@
-module.exports.home = function (req, res) {
-	// return res.redire();
+const Posts = require("../models/post");
 
-	console.log(req.cookies);
-	return res.render("home", {
-		title: "Home Page",
-	});
+module.exports.home = function (req, res) {
+	// // fetching all posts
+	// Posts.find({}, (error, posts) => {
+	// 	if (error) {
+	// 		console.log("error while feching posts");
+	// 		return;
+	// 	}
+
+	// 	// render home page and sending all user posts
+	// 	return res.render("home", {
+	// 		title: "Home Page",
+	// 		posts,
+	// 	});
+	// });
+	// fetching all posts and populating user
+	Posts.find({})
+		.populate("user")
+		.exec((error, posts) => {
+			if (error) {
+				console.log("error while feching posts");
+				return;
+			}
+
+			// render home page and sending all user posts
+			return res.render("home", {
+				title: "Home Page",
+				posts,
+			});
+		});
 };
